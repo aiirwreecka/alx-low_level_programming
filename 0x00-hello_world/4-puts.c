b46c75160cb8bd951cebda91878dc82fdf6a10c9
“aiirwreecka” <ericarossanamateus@gmail.com>
2022-04-07 16:07:44 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - entry point 
+ * main - entry point "Programming is like building a multilingual puzzle" 
  * Return 0 after printing the function
  */
 int main(void)
