53787d81eb9c65a9bb67eb9e3cf5cce50291910b
“aiirwreecka” <ericarossanamateus@gmail.com>
2022-04-11 20:42:09 -0700

    Smile

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success/correct)
+ */
+
+int main(void)
+{
+	char smile;
+	
+	for (smile = 122 ; smile >= 97; smile++)
+	{
+		putchar(smile);
+	}
+
+	putchar('\n')
+
+		return (0);
+}
