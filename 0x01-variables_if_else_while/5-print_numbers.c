5d4ea2f38c17032d7c26e81a7a7b53d8cde0bfc2
“aiirwreecka” <ericarossanamateus@gmail.com>
2022-04-11 20:19:23 -0700

    Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - Prits the number 0 to 9
  *
  * Return: Always 0 (Success/correct)
  */
 
 int main(void)
 {
-	int digits;
+	int d;
 
-	for (digit = 0 ; digit < 10; digit++)
+	for (d = 0 ; d < 10; d++)
 	{
-		printf("%digit", digit);
+		printf("%d", d);
 	}
 
 printf("\n");
