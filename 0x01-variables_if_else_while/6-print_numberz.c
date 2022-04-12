03b103b2c0242b39ea09d29db250928e8667b049
“aiirwreecka” <ericarossanamateus@gmail.com>
2022-04-11 20:29:05 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints number from 0 to 9
+ *
+ * Return: Always (Success/correct)
+ */
+
+int main(void)
+{
+	int d;
+	for (d = '0'; n <= '9'; d++)
+	{
+	putchar(d);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
