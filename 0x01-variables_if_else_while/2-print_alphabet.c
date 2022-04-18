c790e4966182b1628165ac4fa759e8d40e21cb67
“aiirwreecka” <ericarossanamateus@gmail.com>
2022-04-18 03:20:29 -0700

    Alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Print the alphabet in lower case
+ *
+ * Return: Always 0 (Success/Correct)
+ */
+
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet - 'a'; alphabet <= 'z', alphabet++);
+	{
+		putchar(alphabet);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
