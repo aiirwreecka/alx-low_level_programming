474aecbed16627f22e6bf7a05e50ae444685a575
aiirwreecka <89561272+aiirwreecka@users.noreply.github.com>
2022-04-18 19:29:01 +0200

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,35 +1,41 @@
 #include <stdio.h>
-
 /**
- * main - Prints all possible different combinations for two digits
+ * main - Entry point
  *
- * Return: Always 0 (Success/correction)
+ * Return: Always 0 (Success/completed)
  */
-
 int main(void)
+
 {
-	int number_left;
-	int number_right;
+int number_left;
+int number_right;
 
-	for (number_left = 56; number_right <= 78; number_left++)
-	{
 
-		for (number_right = number_left + 1 ; number_right <= 78; number_right++)
-		{
-			putchar(number_left);
-			putchar (number_right);
+for (number_left = 48; number_right <= 78; number_left++)
+{
+for (number_right = number_left + 1 ; number_right <= 78; number_right++)
+{
 
-			if ((number_left == 60) && (number_right == 78))
-			{
-				break;
-			}
+putchar(number_left);
+putchar (number_right);
 
-			putchar(',');
-			putchar (' ');
-		}
-	}
 
-	putchar('\n');
+if ((number_left == 56) && (number_right == 78))
+{
+break;
+}
 
-	return (0);
+putchar(',');
+putchar (' ');
+
+}
+
+}
+
+putchar('\n');
+
+
+return (0);
+
+
 }
