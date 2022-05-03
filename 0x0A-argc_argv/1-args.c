a7f31de469ebe5db8832f5f1f8e5dacff048097a
aiirwreecka <89561272+aiirwreecka@users.noreply.github.com>
2022-05-03 16:11:35 +0200

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
-#include "main.h"
+#include <stdlib.h>
 
 /**
- * main - print the name of the program
- * @argc: Count arguments
- * @argv: Arguments
+ * main - Entry point
  *
+ * @argc: Counts the number of parameters that go into main
+ * @argv: Pointer of array of pointers containing strings entering main
  * Return: Always 0 (Success)
  */
-
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-    (void)argv; /*Ignore argv*/
-    printf("%i\n", argc - 1);
-
-    return (0);
+	(void) argv;
+	printf("%i\n", argc - 1);
+	return (0);
 }
