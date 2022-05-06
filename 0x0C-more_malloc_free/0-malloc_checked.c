7af6c146f90b05dc3469cc377667f1037561da3c
aiirwreecka <89561272+aiirwreecka@users.noreply.github.com>
2022-05-06 14:14:00 +0200

    Update 0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,32 +1,21 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * create_array - creates an array of chars, and initializes it with
- * a specific char
- * @size: size of array
- * @c: character to initialize with
- *
- * Return: Pointer
- */
-char *create_array(unsigned int size, char c)
+ *malloc_checked - allocate memory.
+ *@b: size of the memory to be allocated.
+ *Return: pointer;
+ **/
+
+void *malloc_checked(unsigned int b)
 {
-	unsigned int i;
-	char *s;
+	char *p;
 
-	if (size <= 0)
-		return (0);
-
-	s = malloc(sizeof(char) * size);
-
-	if (s == 0)
-		return (0);
-
-	for (i = 0; i < size; i++)
-		*(s + i) = c;
-
-	*(s + i) = '\0';
-
-	return (s);
+	p = malloc(b);
+	if (p == NULL)
+	{
+		exit(98);
+		return (NULL);
+	}
+	return (p);
 }
