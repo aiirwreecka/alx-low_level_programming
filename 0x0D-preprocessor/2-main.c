be919edc1e46fe399093b18b9fdeff7fb705f942
aiirwreecka <89561272+aiirwreecka@users.noreply.github.com>
2022-05-09 16:50:27 +0200

    main

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,3 +1,12 @@
-#ifndef PI
-#define PI 3.14159265359
-#endif
+#include <stdio.h>
+
+/**
+ * main - Print the the name of the file it was compiled from.
+ * Return: success (0).
+ **/
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
